ea699d11b476bcfecbb974996860e9593fc3fe1c
Hellden <r.chapotin@gmail.com>
2020-04-05 21:48:03 +0200

    .

--- backend/app.js
+++ backend/app.js
@@ -23,14 +23,32 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 
 app.post('/api/stuff', (req, res, next) => {
-  console.log("Req", req);
-
   delete req.body._id;
   const thing = new Thing({
     ...req.body
   });
+  console.log("Thing", thing);
   thing.save()
     .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
     .catch(error => res.status(400).json({ error }));
 });
 
